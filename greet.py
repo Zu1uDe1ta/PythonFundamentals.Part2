c9546fea961bc790d5f683da38ac533b95f7271b
Zu1u De1ta <C.Chavez@me.com>
2020-03-18 06:02:00 -0400

    Lab Reviewed

--- greet.py
+++ greet.py
@@ -1,14 +1,16 @@
 def name_input():
-"""
-WILL PROVIDE THE PROMPT TO INPUT NAME
-"""
-  name = input("What is your name?!")
-  return(name)
+    """
+    WILL PROVIDE THE PROMPT TO INPUT NAME
+    """
+    name = input("What is your name?!\n ")
+    return (name)
+
 
 def greet(name):
-"""
-WILL GIVE THE GREET WITH THE PREVIOUS NAME
-"""
+    """
+    WILL GIVE THE GREET WITH THE PREVIOUS NAME
+    """
     print("Hello " + name)
+
 
 greet(name_input())
